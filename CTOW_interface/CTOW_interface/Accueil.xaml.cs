1658eb77e4fcb105bd323fab5eca327860c96892
Massimo GENTILE <massimo.gentile@condorcet.be>
2018-03-10 18:44:10 +0100

    Ajout d'un accueil et de la partie audite non terminée

---
+++ CTOW_interface/CTOW_interface/Accueil.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CTOW_interface
+{
+    /// <summary>
+    /// Logique d'interaction pour Accueil.xaml
+    /// </summary>
+    public partial class Accueil : Window
+    {
+        public Accueil()
+        {
+            InitializeComponent();
+        }
+
+        private void Audite(object sender, RoutedEventArgs e)
+        {
+            Audite audite = new Audite();
+            this.Close();
+            audite.Show();            
+        }
+
+        private void Service(object sender, RoutedEventArgs e)
+        {
+            MainWindow service = new MainWindow();
+            this.Close();
+            service.Show();
+        }
+    }
+}
